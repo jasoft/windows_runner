107b0240eddac27e617fc3c6a4a9f4c79c1fb3e7
John Anders <ja.invent@gmail.com>
2025-11-15 14:54:32 +0800

    Delete dist directory

--- dist/config.d.ts
+++
@@ -1,7 +0,0 @@
-export declare const config: {
-    port: number;
-    allowedCommands: string[] | undefined;
-    defaultTimeoutMs: number | undefined;
-    maxOutputLength: number;
-};
-//# sourceMappingURL=config.d.ts.map
